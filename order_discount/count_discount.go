ba5f2b575b1524eec5701be1c30b467cd7a14d42
Firyal-dev <firyal.dev@gmail.com>
2026-07-08 06:29:58 +0700

    feat:implement count discount and validate maks discount function

--- order_discount/count_discount.go
+++ order_discount/count_discount.go
@@ -1,2 +1,30 @@
 package main
 
+// menghitung diskon belanja dan diskon member
+func HitungDiskonBelanja(belanja int, isMember bool) (int, int, int) {
+	diskonBelanja := 0
+	diskonMember := 0
+	minBelanja := 100000
+
+	if belanja > minBelanja {
+		diskonBelanja = belanja * 10 / 100 // diskon 10%
+	}
+	if isMember {
+		diskonMember = belanja * 5 / 100 // diskon 5% untuk member
+	}
+
+	totalDiskon := diskonBelanja + diskonMember
+
+	maxDiskon := MaksDiskon(belanja)
+	if totalDiskon > maxDiskon {
+		totalDiskon = maxDiskon
+	}
+
+	return diskonBelanja, diskonMember, totalDiskon
+}
+
+// menghitung maks diskon (30%)
+func MaksDiskon(belanja int) int {
+	maxDiskon := belanja * 30 / 100
+	return maxDiskon
+}
